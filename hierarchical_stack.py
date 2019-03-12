3f3d3bd8c6a86eb06e55089945270cdf2b311527
Matthew Harding (SC) Part III Jan 2019 <mh826@syrma.esc.cam.ac.uk>
2019-03-12 11:26:51 +0000

    Cleaning up and running

--- hierarchical_stack.py
+++ hierarchical_stack.py
@@ -11,11 +11,6 @@ import mpl_toolkits.basemap
 from mpl_toolkits.basemap import Basemap
 from collections import Counter
 
-# Clustering and stacking data
-# Clusters using data from b
-# Then stacks the data in a
-# Returns stacked data and list of cluster indices
-
 # Returns L2 correlation between traces tr1 and tr2
 
 def correlation(tr1, tr2, n=2):
@@ -29,7 +24,7 @@ def group_corr(traces, stack):
 # Haversine distance formula
 
 def haversine(coord1, coord2):
-
+	
 	lat1, long1 = coord1*(pi/180)
 	lat2, long2 = coord2*(pi/180)
 	del_lat = lat2 - lat1
@@ -37,12 +32,13 @@ def haversine(coord1, coord2):
 	R = 6371
 	a = sin(del_lat/2)**2 + cos(lat1)*cos(lat2)*(sin(del_long/2)**2)
 	c = 2*atan2(sqrt(a), sqrt(1-a))
-
+	
 	return R*c
 
 # 'Distance' metric to use for clustering - can weight by distance, correlation, or both
 
 def combined_metric(a, b, include_dist = True, include_corr = False):
+	
 	if (include_corr == False):
 		return haversine(a[-2:], b[-2:])
 	elif (include_dist == False):
@@ -56,68 +52,85 @@ def combined_metric(a, b, include_dist = True, include_corr = False):
 # that the average of a cluster is the 'typical' point, rather than the centroid, or another more complicated metric 
 
 def stack_coords(cluster, coords):
-
+	
 	clusters = np.arange(1,np.max(cluster).astype(int)+1)
 	coords = np.array([[np.average([coords[i][j] for i in np.where(cluster == cl)[0]]) for j in [0, 1]] for cl in clusters])
 	coords = np.nan_to_num(coords)
-	print(len(coords))
-	print(len(clusters))
-
+	
 	return coords
 
-def plot(stacks, depths, cluster, coords, seis_data, figname, anomal=True, plot_individual = False):
+# Create map of western US to overlay location points on
 
+def create_map(lon, lat):
+	
+	m = Basemap(llcrnrlon=np.min(lon)-5.,llcrnrlat=np.min(lat)-5.,urcrnrlon=np.max(lon)+5.,urcrnrlat=np.max(lat)+5.,
+        resolution='i',projection='merc',lon_0=np.mean(lon),lat_0=np.mean(lat))
+	m.shadedrelief()
+	m.drawparallels(np.arange(-40,80.,5.),color='gray')
+	m.drawmeridians(np.arange(-30.,80.,5.),color='gray')
+	
+	return m
+
+# Plots final stacks with and cluster locations.
+# anomal - if false, exclude all stacks with less than 400 data points for clarity
+# plot_indiviudal - if true, plots every individual stack in its own figure
+
+def plot(stacks, depths, cluster, coords, seis_data, figname, anomal=True, plot_individual = False):
+	
+	# Creating a random set of colours to distinguish between different clusters
+	
 	colours = []
 	for i in range(1, np.max(cluster).astype(int) +1):
 		a = '%06X' % randint(0, 0xFFFFFF)
 		b = '#' + a
 		colours.append(b)
 	colour_clusters = [colours[(cluster[j]-1).astype(np.int)] for j in range(len(cluster))]
+	
+	# Initialising figure
+	
 	plt.figure(1)
 	fig, axes = plt.subplots(1,2)
 	ax1 = axes[0]
 	ax2 = axes[1]
-	lon410 = np.array([co[1] for co in coordinates])
-	lat410 = np.array([co[0] for co in coordinates])
-
+	lons = np.array([co[1] for co in coords])
+	lats = np.array([co[0] for co in coords])
+	
+	# Plotting individual stacks, if plot_individual is true
+	
 	if plot_individual == True:
 		count = 0
 		for stack in stacks:
 			if (anomal == False and len(np.where(cluster == count+1)[0])<400):
 				count+=1
 			else:
-				ax2.set_xlabel('longitude')
-				ax2.set_ylabel('latitude')
-				lon410 = np.array([co[1] for co in coordinates])
-				lat410 = np.array([co[0] for co in coordinates])
-				m = Basemap(llcrnrlon=np.min(lon410)-5.,llcrnrlat=np.min(lat410)-5.,urcrnrlon=np.max(lon410)+5.,urcrnrlat=np.max(lat410)+5.,
-				resolution='i',projection='merc',lon_0=np.mean(lon410),lat_0=np.mean(lat410))
-				m.shadedrelief()
-				m.drawparallels(np.arange(-40,80.,5.),color='gray')
-				m.drawmeridians(np.arange(-30.,80.,5.),color='gray')
 				
 				ax1.set_xlabel('depth (km)')
 				ax1.set_ylabel('amplitude relative to main P wave')
-				inds = np.where(cluster == count+1)[0]
 				ax1.plot(depths, stack, color = colours[count])
-
+				
+				ax2.set_xlabel('longitude')
+				ax2.set_ylabel('latitude')
+				m = create_map(lons, lats)
+				inds = np.where(cluster == count+1)[0]
 				for i in inds:
 					lon, lat = m(coords[i][1], coords[i][0])
 					m.plot(lon, lat, marker='x', markeredgecolor = colour_clusters[i])
 				count+=1
+				
 				fig.savefig(figname + str(count))
 				ax1.clear()
 				ax2.clear()
+	
+	# Initialising final figure
 	
 	ax1.set_xlabel('depth (km)')
 	ax1.set_ylabel('amplitude relative to main P wave')
 	ax2.set_xlabel('longitude')
 	ax2.set_ylabel('latitude')
-	m = Basemap(llcrnrlon=np.min(lon410)-5.,llcrnrlat=np.min(lat410)-5.,urcrnrlon=np.max(lon410)+5.,urcrnrlat=np.max(lat410)+5.,
-	resolution='i',projection='merc',lon_0=np.mean(lon410),lat_0=np.mean(lat410))
-	m.shadedrelief()
-	m.drawparallels(np.arange(-40,80.,5.),color='gray')
-	m.drawmeridians(np.arange(-30.,80.,5.),color='gray')
+	m = create_map(lons, lats)
+
+	# Plotting final figure
+	
 	count = 0
 	for stack in stacks:
 		if (anomal == False and len(np.where(cluster == count+1)[0])<400):
@@ -125,15 +138,25 @@ def plot(stacks, depths, cluster, coords, seis_data, figname, anomal=True, plot_
 		else:
 			ax1.plot(depths, stack, color = colours[count])
 			count+=1
-	
 	for i in range(len(coords)):
 		lon, lat = m(coords[i][1], coords[i][0])
 		m.plot(lon, lat, marker='x', markeredgecolor=colour_clusters[i])
-	
 	fig.savefig(figname)
-	fig.show()
 	
-def depth_plot(cluster, stacks, coords, depths, figname, fourten=True):
+# Plots depth of peak at a certain lon, lat as a colourmap
+
+def depth_plot(cluster, stacks, coords, depths, min_depth, max_depth, figname):
+	
+	# Find peaks
+	
+	cut_index_1 = np.argmax(depths>min_depth)
+	cut_index_2 = np.argmax(depths>max_depth)
+	cropped_depths = depths[cut_index_1:cut_index_2]
+	cropped_stacks = [stack[cut_index_1:cut_index_2] for stack in stacks]
+	signal_depths = [cropped_depths[np.argmax(stack)] for stack in cropped_stacks]
+	coord_depths = [signal_depths[cluster[i]-1] for i in range(len(coords))]
+	
+	# Plot figure
 	
 	fig = plt.figure(1)
 	axes = plt.gca()
@@ -141,20 +164,30 @@ def depth_plot(cluster, stacks, coords, depths, figname, fourten=True):
 	axes.set_ylabel('latitude')
 	lon = [coords[i][1] for i in range(len(coords))]
 	lat = [coords[i][0] for i in range(len(coords))]
-	if fourten==True:
-		cut_index = np.argmax(depths>390)
-	else:
-		cut_index = np.argmax(depths>630)
-
-	cropped_depths = depths[cut_index:]
-	cropped_stacks = [stack[cut_index:] for stack in stacks]
-	sig_depths = [cropped_depths[np.argmax(stack)] for stack in cropped_stacks]
-	coord_depths = [sig_depths[cluster[i]-1] for i in range(len(coords))]
 	cb = axes.scatter(lon, lat, c=coord_depths, marker='x', cmap=plt.cm.get_cmap('cool'))
 	fig.colorbar(cb, ax=axes)
 	fig.savefig(figname)
 
-def var_plot(cluster, stacks, coords, seis_data, figname):
+# Plots thickness of MTZ as colormap
+
+def MTZ_plot(cluster, stacks, coords, depths, figname):
+	
+	# Find peaks and therefore MTZ thickness
+	
+	cut_index_1 = np.argmax(depths>370)
+	cut_index_2 = np.argmax(depths>470)
+	cut_index_3 = np.argmax(depths>610)
+	cut_index_4 = np.argmax(depths>710)
+	cropped_depths_1 = depths[cut_index_1:cut_index_2]
+	cropped_stacks_1 = [stack[cut_index_1:cut_index_2] for stack in stacks]
+	cropped_depths_2 = depths[cut_index_3:cut_index_4]
+	cropped_stacks_2 = [stack[cut_index_3:cut_index_4] for stack in stacks]
+	signal_depths_1 = [cropped_depths_1[np.argmax(stack)] for stack in cropped_stacks_1]
+	signal_depths_2 = [cropped_depths_2[np.argmax(stack)] for stack in  cropped_stacks_2]
+	MTZ_widths = [j - i for i, j in np.stack((signal_depths_1, signal_depths_2), axis = -1)]
+	coord_widths = [MTZ_widths[cluster[i]-1] for i in range(len(coords))]
+	
+        # Plot figure
 	
 	fig = plt.figure(1)
 	axes = plt.gca()
@@ -162,6 +195,15 @@ def var_plot(cluster, stacks, coords, seis_data, figname):
 	axes.set_ylabel('latitude')
 	lon = [coords[i][1] for i in range(len(coords))]
 	lat = [coords[i][0] for i in range(len(coords))]
+	cb = axes.scatter(lon, lat, c=coord_widths, marker='x', cmap=plt.cm.get_cmap('cool'))
+	fig.colorbar(cb, ax=axes)
+	fig.savefig(figname)
+
+	return
+
+def var_plot(cluster, stacks, coords, seis_data, figname):
+	
+	# Find cluster variances
 	
 	vars = np.zeros(len(stacks))
 	count = 1
@@ -170,45 +212,81 @@ def var_plot(cluster, stacks, coords, seis_data, figname):
 		vars[count-1] = group_corr(orig, stack)
 		count += 1
 	coord_vars = [vars[cluster[i]-1] for i in range(len(coords))]
-
+	
+	# Plot figure
+	
+	fig = plt.figure(1)
+	axes = plt.gca()
+	axes.set_xlabel('longitude')
+	axes.set_ylabel('latitude')
+	lon = [coords[i][1] for i in range(len(coords))]
+	lat = [coords[i][0] for i in range(len(coords))]
 	cb = axes.scatter(lon, lat, c=coord_vars, marker='x', cmap=plt.cm.get_cmap('cool'))
 	fig.colorbar(cb, ax=axes)
 	fig.savefig(figname)
 
+# Clustering and stacking data
+# Clusters using distances between data in b (b is combined trace data and coordinates)
+# Then stacks the data in a according to resulting clusters (a is just trace data)
+# Threshold is clustering threshold, crit is clustering criterion
+# Returns stacked data and list of cluster indices
+
 def cluster_data(a, b, metric, threshold, crit):
+	
+	# Clustering and stacking
+	
 	cluster = sp.cluster.hierarchy.fclusterdata(b, t=threshold, criterion=crit, metric=metric, method='average')
 	stacks = np.array([[np.sum([a[i][j] for i in np.where(cluster == cl+1)[0]]) for j in range(len(a[0]))] for cl in range(np.max(cluster))])
+	
+	# Renomalising stacks
+	
 	for cl in range(1, np.max(cluster+1)):
 		length = len(np.where(cluster == cl)[0])
 		stacks[cl-1] = stacks[cl-1]/length
 	stacks = np.nan_to_num(stacks)
+
 	return cluster, stacks
+
+# Wrapper function for the clusterer
+# Returns cluster data extended to correspond to each individual data point
 
 def second_cluster(cluster, coordinates, stacks, threshold, crit, dist = True, corr = False):
 	
-	# Formatting data
+	# Formatting data - adding coordinates on end of trace data and creating 'distance' metric
+	
 	comb_metric = (lambda a, b: combined_metric(a, b, include_dist=dist, include_corr=corr))
 	new_data = np.append(stacks, coordinates, axis=1)
+	
 	# Clustering data
+	
 	cluster_2, stacks = cluster_data(new_data, new_data, comb_metric, threshold, crit)
-	# Running through old cluster and re-formatting data
+	
+	# Reformatting cluster to correspond to original data points
+	
 	new_cluster = np.zeros(len(cluster))
 	for cl in np.arange(1, np.max(cluster).astype(int)+1):
         	new_cluster[np.where(cluster == cl)] = cluster_2[cl-1].astype(int)
-	stacks = stacks[:, :-2]
 	cluster = new_cluster.astype(int)
+	
+	# Cutting coordinates off the stacks
+	
+	stacks = stacks[:, :-2]
+	
+	return cluster, stacks
 
-	return cluster, stacks, cluster_2
-
-file =sys.argv[1]
-print("Reading " + file + "...")
-seis = read(file,format='PICKLE')
-
-# Formatting relevant data
-length = len(seis[0].data)
-seis_data = np.array([tr.data for tr in seis])
-loc410 = np.array([t.stats.piercepoints['P410s']['410'] for t in seis]).astype(float)
-coordinates = np.array([(l410[1], l410[2]) for l410 in loc410])
-depths = np.array(seis[0].depth)
-coordinates = coordinates[:, :]
-seis_data = seis_data[:, :]
+def set_up():
+	
+	# Reading in
+	
+	file =sys.argv[1]
+	print("Reading " + file + "...")
+	seis = read(file,format='PICKLE')
+	
+	# Formatting relevant data
+	
+	seis_data = np.array([tr.data for tr in seis])
+	locs = np.array([t.stats.piercepoints['P410s']['410'] for t in seis]).astype(float)
+	coordinates = np.array([(l[1], l[2]) for l in locs])
+	depths = np.array(seis[0].stats.depth)
+	
+	return seis_data, coordinates, depths
--- stacking_script.py
+++ stacking_script.py
@@ -17,7 +17,10 @@ from mpl_toolkits.basemap import Basemap
 import csv
 import plot_CCP
 
+# Write out cluster and stacks to a csv
+
 def print_out(cluster, stacks, filename):
+
 	file = open(filename, 'w')
 	for c in cluster:
 		file.write(str(c) + ', ')
@@ -27,9 +30,13 @@ def print_out(cluster, stacks, filename):
 			file.write(str(s) + ', ')
 		file.write('\n')
 
+	return
+
+# Read in cluster and stacks from a csv
+
 def read_in(no_stacks, filename):
 	with open(filename) as csv_file:
-		stacks = np.zeros((no_stacks,len(hs.seis_data[0])))
+		stacks = np.zeros((no_stacks,len(seis_data[0])))
 		csv_reader = csv.reader(csv_file, delimiter=',')
 		line_count = 0
 		for row in csv_reader:
@@ -42,7 +49,12 @@ def read_in(no_stacks, filename):
 				line_count += 1
 	return cluster, stacks
 
+# Remove all stacks containing fewer data points than a given size from the clusterer
+
 def remove_anoms(cluster, stacks, coords, seis_data, size):
+	
+	# Making list of those to remove
+	
 	to_remove = np.array([])
 	remove_cluster = np.array([])
 	for c in np.arange(np.max(cluster)):
@@ -50,15 +62,22 @@ def remove_anoms(cluster, stacks, coords, seis_data, size):
 		if (len(a) < size):
 			to_remove = np.append(to_remove, [c], axis=0)
 			remove_cluster = np.append(remove_cluster, a, axis=0)
+	
+	# Removing all anomalies
+	
 	stacks = np.delete(stacks, to_remove.astype(int), axis=0)
 	cluster = np.delete(cluster, remove_cluster.astype(int))
+	coords = np.delete(coords, remove_cluster.astype(int), axis=0)
+	seis_data = np.delete(seis_data, remove_cluster.astype(int), axis=0)
+	
+	# Rebuilding cluster
+	
 	cluster_set = set(cluster)
 	count = 1
 	for c in cluster_set:
 		cluster[np.where(cluster==c)[0]] = count
 		count += 1
-	coords = np.delete(coords, remove_cluster.astype(int), axis=0)
-	seis_data = np.delete(seis_data, remove_cluster.astype(int), axis=0)
+	
 	return cluster, stacks, coords, seis_data
 
 def compare_methods(cluster, stacks, coords, seis_data, figname):
@@ -74,7 +93,7 @@ def compare_methods(cluster, stacks, coords, seis_data, figname):
 		colours.append(b)
 	plt.figure(1)
 	fig, axes = plt.subplots(1,2)
-	d, profiles = plot_CCP.retrieve_profiles(avg_coords, 280, 480)
+	d, profiles = plot_CCP.retrieve_profiles(avg_coords, np.min(depths), np.max(depths))
 	diffs = np.zeros(len(avg_coords))
 	count = 0
 	for avg_coord in avg_coords:
@@ -90,7 +109,6 @@ def compare_methods(cluster, stacks, coords, seis_data, figname):
 		count += 1
 	fig.savefig(figname + '_all')
 	sizes = [len(np.where(cluster==cl)[0]) for cl in range(1, np.max(cluster)+1)]
-	print(sizes)
 	to_examine = np.where(diffs>0.00)[0]
 	fig1, axes1 = plt.subplots(1, 2)
 	ax1 = axes1[0]
@@ -105,7 +123,7 @@ def compare_methods(cluster, stacks, coords, seis_data, figname):
 		m.shadedrelief()
 		m.drawparallels(np.arange(-40,80.,5.),color='gray')
 		m.drawmeridians(np.arange(-30.,80.,5.),color='gray')
-	
+		
 		ax1.set_xlabel('depth (km)')
 		ax1.set_ylabel('amplitude relative to main P wave')
 		inds = np.where(cluster == cl+1)[0]
@@ -118,27 +136,31 @@ def compare_methods(cluster, stacks, coords, seis_data, figname):
 		fig1.savefig(figname + str(cl+1))
 		ax1.clear()
 		ax2.clear()
-	fig2 = plt.figure()
-	plt.xlabel('size of cluster')
-	plt.ylabel('diff between methods')
-	plt.plot(sizes, diffs, linestyle='None', marker='x')
-	fig2.savefig(figname + '_size')
 	return
 
-print("Assign...")
-coords = hs.coordinates
-seis_data = hs.seis_data
-depths = hs.depths
+min_depth = 280
+max_depth = 750
+seis_data, coordinates, depths = hs.set_up()
+cut_index_1 = np.where(depths>min_depth)[0][0]
+cut_index_2 = np.where(depths>max_depth)[0][0]
+cluster, stacks = read_in(1750, 'stack_data_whole_range.csv')
+depths = np.array(depths[cut_index_1:cut_index_2])
+seis_data = np.array([seis[cut_index_1:cut_index_2] for seis in seis_data])
+stacks = np.array([stack[cut_index_1:cut_index_2] for stack in stacks])
+
 print("Stacking...")
-cluster, stacks = read_in(1750, 'stack_data_1.csv')
-cluster, stacks, coords, seis_data = remove_anoms(cluster, stacks, hs.coordinates, seis_data, 10)
-cluster, stacks, short_cluster = hs.second_cluster(cluster, hs.stack_coords(cluster,coords), stacks, threshold=200, crit='maxclust', dist = True, corr = True)
+
+cluster, stacks, coords, seis_data = remove_anoms(cluster, stacks, coordinates, seis_data, 10)
+cluster, stacks = hs.second_cluster(cluster, hs.stack_coords(cluster,coords), stacks, threshold=200, crit='maxclust', dist = True, corr = True) #true, true
 cluster, stacks, coords, seis_data = remove_anoms(cluster, stacks, coords, seis_data, 20)
-cluster, stacks, short_cluster = hs.second_cluster(cluster, hs.stack_coords(cluster,coords), stacks, threshold = 1, crit='inconsistent', dist = True, corr = False)
-cluster, stacks, short_cluster = hs.second_cluster(cluster, hs.stack_coords(cluster,coords), stacks, threshold = 1, crit = 'inconsistent', dist = True, corr = True)
+cluster, stacks = hs.second_cluster(cluster, hs.stack_coords(cluster,coords), stacks, threshold = 1, crit='inconsistent', dist = True, corr = False) #true, false
+cluster, stacks = hs.second_cluster(cluster, hs.stack_coords(cluster,coords), stacks, threshold = 1, crit = 'inconsistent', dist = True, corr = True) #true, true
 cluster, stacks, coords, seis_data = remove_anoms(cluster, stacks, coords, seis_data, 100)
+
 print("Plotting...")
-#hs.plot(stacks, depths, cluster, coords, seis_data, "default", anomal = False, plot_individual = True)
-#hs.depth_plot(cluster, stacks, coords, depths, 'default_depths', fourten=True)
-#hs.var_plot(cluster, stacks, coords, seis_data, 'default_vars')
-#compare_methods(cluster, stacks, coords, seis_data, 'compare')
+
+hs.plot(stacks, depths, cluster, coords, seis_data, "whole_range", anomal = False, plot_individual = True)
+hs.MTZ_plot(cluster, stacks, coords, depths, 'MTZ')
+#hs.depth_plot(cluster, stacks, coords, depths, 350, 450, 'corr_depths')
+hs.var_plot(cluster, stacks, coords, seis_data, 'whole_range_vars')
+compare_methods(cluster, stacks, coords, seis_data, 'whole_range_compare')
